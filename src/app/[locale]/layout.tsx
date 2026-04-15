72cf876a42a59d190512455e3a70a976ab36874b
Andres Sarria <andres.sarria@gmail.com>
2026-04-15 18:25:53 +0200

    feat: install Google Tag Manager

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
+import Script from 'next/script';
 import "../globals.css";
 import { NextIntlClientProvider } from 'next-intl';
 import { getMessages } from 'next-intl/server';
@@ -121,10 +122,29 @@ export default async function RootLayout({
 
   return (
     <html lang={locale} suppressHydrationWarning data-scroll-behavior="smooth" className="relative">
+      <head>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PJDR9NCR');
+          `}
+        </Script>
+      </head>
       <body
         className={`${outfit.variable} antialiased bg-background text-foreground overflow-x-hidden`}
         suppressHydrationWarning
       >
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PJDR9NCR"
+            height="0" 
+            width="0" 
+            style={{ display: 'none', visibility: 'hidden' }} 
+          />
+        </noscript>
         <StructuredData locale={locale} />
         <NextIntlClientProvider messages={messages}>
           {children}
